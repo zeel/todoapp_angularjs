0216ed590b48c0d587ae13ab92dd2ef5a83c8804
zeel <kshah215@gmail.com>
2015-09-09 17:57:59 +0530

    completed with integartion of travis

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -9,9 +9,9 @@ exports.config = {
     browserName: 'chrome'
   }],
   chromeOnly: true,
-  baseUrl: 'http://localhost/todos/',
-  seleniumServerJar:'/usr/lib/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
-  chromeDriver : '/usr/lib/node_modules/protractor/selenium/chromedriver',
+  baseUrl: 'http://localhost:8000',
+  // seleniumServerJar:'/usr/lib/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+  // chromeDriver : '/usr/lib/node_modules/protractor/selenium/chromedriver',
   framework: 'jasmine2',
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
